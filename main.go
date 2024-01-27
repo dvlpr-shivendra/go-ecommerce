d66c80db663a7c5e033fdb661a775b7447dc8cb9
Shivendra TechSter <shivendratechSter@gmail.com>
2024-01-28 01:15:47 +0530

    Use env file

--- main.go
+++ main.go
@@ -1,12 +1,59 @@
 package main
 
 import (
-  "net/http"
+	"fmt"
+	"net/http"
 
-  "github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/gin-gonic/gin"
+
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+type Product struct {
+  gorm.Model
+  Title string
+  Slug string
+  Price uint
+}
+
 func main() {
+  err := godotenv.Load()
+  if err != nil {
+    panic("Error loading .env file")
+  }
+
+  dbHost := os.Getenv("DB_HOST")
+  dbUser := os.Getenv("DB_USER")
+  dbPassword := os.Getenv("DB_PASSWORD")
+  dbName := os.Getenv("DB_Name")
+  dbPort := os.Getenv("DB_PORT")
+
+  dsn := fmt.Sprintf(
+    "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+    dbHost, dbUser, dbPassword, dbName, dbPort,
+  )
+  
+  db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+  if err != nil {
+    panic(err);
+  }
+
+  // Migrate the schema
+  db.AutoMigrate(&Product{})
+
+  // Create
+  db.Create(&Product{
+    Title: "Airpods Pro 2nd Generation",
+    Slug: "airpods-pro-2nd-generation",
+    Price: 100,
+  })
+
   r := gin.Default()
   r.GET("/ping", func(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{
