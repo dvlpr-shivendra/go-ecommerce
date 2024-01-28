665a88805f9482453632314808481d50d1835aa5
Shivendra TechSter <shivendratechSter@gmail.com>
2024-01-28 11:37:35 +0530

    remove slug, use lowercase field names in JSON for product

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-  "strconv"
 	"net/http"
+	"strconv"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -16,46 +17,48 @@ import (
 )
 
 type Product struct {
-  gorm.Model
-  Title string
-  Slug string
-  Price uint
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	Title     string         `gorm:"not null" json:"title"`
+	Price     uint           `json:"price"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
 
 func main() {
-  err := godotenv.Load()
-  if err != nil {
-    panic("Error loading .env file")
-  }
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
 
-  dbHost := os.Getenv("DB_HOST")
-  dbUser := os.Getenv("DB_USER")
-  dbPassword := os.Getenv("DB_PASSWORD")
-  dbName := os.Getenv("DB_Name")
-  dbPort := os.Getenv("DB_PORT")
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_Name")
+	dbPort := os.Getenv("DB_PORT")
 
-  dsn := fmt.Sprintf(
-    "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-    dbHost, dbUser, dbPassword, dbName, dbPort,
-  )
-  
-  db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		dbHost, dbUser, dbPassword, dbName, dbPort,
+	)
 
-  if err != nil {
-    panic(err);
-  }
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-  // Migrate the schema
-  db.AutoMigrate(&Product{})
+	if err != nil {
+		panic(err)
+	}
 
-  r := gin.Default()
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
 
-  r.Use(func(c *gin.Context) {
+	r := gin.Default()
+
+	r.Use(func(c *gin.Context) {
 		c.Header("Content-Type", "application/json")
 		c.Next()
 	})
 
-  r.GET("/products", func(c *gin.Context) {
+	r.GET("/products", func(c *gin.Context) {
 		page, _ := strconv.Atoi(c.Query("page"))
 		limit, _ := strconv.Atoi(c.Query("limit"))
 
@@ -73,11 +76,24 @@ func main() {
 		db.Offset(offset).Limit(limit).Find(&products)
 
 		c.JSON(http.StatusOK, gin.H{
-			"page":       page,
-			"limit":      limit,
-			"products":   products,
+			"page":     page,
+			"limit":    limit,
+			"products": products,
 		})
-  })
-  
-  r.Run()
+	})
+
+	r.POST("/products", func(c *gin.Context) {
+		var product Product
+
+		if err := c.BindJSON(&product); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		db.Create(&product)
+
+		c.JSON(http.StatusCreated, product)
+	})
+
+	r.Run()
 }
