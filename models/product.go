8b2cdb9cb7207ec9f1ff7827ca44bc0d27a05dbe
Shivendra TechSter <shivendratechSter@gmail.com>
2024-02-11 10:15:29 +0530

    Product: add description

--- models/product.go
+++ models/product.go
@@ -2,14 +2,16 @@ package models
 
 import (
 	"time"
+
 	"gorm.io/gorm"
 )
 
 type Product struct {
-	ID        uint           `gorm:"primaryKey" json:"id"`
-	Title     string         `gorm:"not null" json:"title"`
-	Price     uint           `json:"price"`
-	CreatedAt time.Time      `json:"createdAt"`
-	UpdatedAt time.Time      `json:"updatedAt"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+	ID          uint           `gorm:"primaryKey" json:"id"`
+	Title       string         `gorm:"not null" json:"title"`
+	Description string         `gorm:"not null" json:"description"`
+	Price       uint           `json:"price"`
+	CreatedAt   time.Time      `json:"createdAt"`
+	UpdatedAt   time.Time      `json:"updatedAt"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
 }
