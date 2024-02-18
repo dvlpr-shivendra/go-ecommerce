b8362d3413e53b186503134bbcc6114ce0beb3fb
Shivendra TechSter <shivendratechSter@gmail.com>
2024-02-18 19:14:32 +0530

    add middleware for CORS

--- api/routes.go
+++ api/routes.go
@@ -13,6 +13,8 @@ func SetupRoutes() {
 		c.Next()
 	})
 
+	r.Use(corsMiddleware())
+
     r.POST("/products", handlers.HandleSaveProduct)
 	r.GET("/products", handlers.HandleGetProducts)
 	r.GET("/products/:id", handlers.HandleGetProductById)
@@ -21,4 +23,20 @@ func SetupRoutes() {
 	r.POST("/auth/login", handlers.HandleLogin)
 	
 	r.Run()
+}
+
+func corsMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
 }
