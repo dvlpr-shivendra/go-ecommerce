94cb7fd06a9e7dbb1bf629f4d8c7fa8570e15574
Shivendra TechSter <shivendratechSter@gmail.com>
2024-04-14 17:04:26 +0530

    Product pagination add max limit

--- handlers/products.go
+++ handlers/products.go
@@ -30,22 +30,26 @@ func HandleGetProducts(c *gin.Context) {
 		page = 1
 	}
 
-	if limit <= 0 {
+	if limit <= 0 || limit > 50 {
 		limit = 10
 	}
 
 	offset := (page - 1) * limit
 
 	var products []models.Product
+	var total int64
 
+	app.Db.Model(&models.Product{}).Count(&total)
 	app.Db.Offset(offset).Limit(limit).Preload("Images").Find(&products)
 
 	c.JSON(http.StatusOK, gin.H{
 		"page":     page,
 		"limit":    limit,
+		"total":    total,
 		"products": products,
 	})
 }
+
 
 func HandleSaveProduct(c *gin.Context) {
 	var product models.Product
